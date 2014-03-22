2f8cf8867065187bc53083f277ef29a5ed71235f
ftao <Filia.Tao@gmail.com>
2014-03-22 18:20:18 +0800

    add command init-ca / gen-key

--- easypki/cli.py
+++ easypki/cli.py
@@ -1,8 +1,32 @@
+#!/usr/bin/env python
+'''
+easypki - Easy PKI Management Tool
+usage:
+  easypki init-ca [<cname>] [options]
+  easypki gen-key (server|client) <cname> [options]
+
+options:
+  --pki-dir=<PKI-DIR>          PKI Directory
+
+'''
+
 import docopt
+import os
+from easypki.pki import get_pki
 
 def main():
-    print 'ok'
-
+    args = docopt.docopt(__doc__)
+    default_pki_dir = os.path.join(os.getcwd(), 'pki')
+    pki_dir = args.get("--pki-dir") or default_pki_dir
+    pki = get_pki(pki_dir)
+    cname = args.get('<cname>')
+    if args.get("init-ca"):
+        pki.init_pki()
+        pki.build_ca(cname)
+    elif args.get("gen-key"):
+        cname = args.get('<cname>')
+        type_ = "server" if args.get('server') else "client"
+        pki.gen_key(type_, cname)
 
 if __name__ == "__main__":
     main()
---
+++ easypki/pki.py
@@ -0,0 +1,49 @@
+'''
+PKI 
+'''
+import os
+import subprocess
+
+class EasyRSAPKI(object):
+    '''
+    An Easy-RSA Based PKI Backend
+    '''
+    def __init__(self, pki_dir, easyrsa_dir):
+        self.pki_dir = pki_dir
+        self.env = {
+            'EASYRSA' : easyrsa_dir,
+            'EASYRSA_PKI' : self.pki_dir,
+            'EASYRSA_DN' : 'cn_only',
+            'EASYRSA_BATCH' : "1",
+        }
+        self.EASYRSA_BIN = os.path.join(self.env['EASYRSA'], 'easyrsa')
+        self._check()
+
+    def _check(self):
+        if not os.path.exists(self.EASYRSA_BIN):
+            raise Exception("%s does not exists" %self.EASYRSA_BIN)
+
+    def init_pki(self):
+        subprocess.call([self.EASYRSA_BIN, "init-pki"], env=self.env)
+
+    def build_ca(self, cname=None):
+        env = self.env.copy()
+        if cname is not None:
+            env.update({'EASYRSA_REQ_CN' : cname})
+        subprocess.call([self.EASYRSA_BIN, "build-ca", 'nopass'], env=env)
+
+    def gen_key(self, type_, cname):
+        env = self.env.copy()
+        if cname is not None:
+            env.update({'EASYRSA_REQ_CN' : cname})
+        #make it  safe for filename 
+        filename = cname
+        subprocess.call([self.EASYRSA_BIN, "gen-req", filename, 'nopass'], env=env)
+        subprocess.call([self.EASYRSA_BIN, "sign-req", type_, filename], env=env)
+
+def get_pki(pki_dir, easyrsa_dir=None):
+    if easyrsa_dir is None:
+        easyrsa_dir = os.environ.get("EASYRSA", None)
+    if easyrsa_dir is None:
+        raise Exception("can not locate EASYRSA tool, consider set environment variable EASYRSA")
+    return EasyRSAPKI(pki_dir, easyrsa_dir)
